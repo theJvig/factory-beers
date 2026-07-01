55b5c2f2e9509996909980ed5b4a4ae0b065fef4
Jacob Vig <jacobvig@gmail.com>
2026-07-01 13:39:01 -0400

    Move git root to theme folder

--- wp-content/themes/factory-beers/build/editor.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => 'b8c0c2a063dad79eb9d8');
--- wp-content/themes/factory-beers/build/main.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '633f44d75bc5ad6dbb82');
